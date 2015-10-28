1a05ea8b38df0bb4ce54213cc103c98f306abf68
Art Rijos <artrijos@yahoo.com>
2015-10-28 02:40:56 -0400

    Testing123

---
+++ ezhttp/src/main/java/com/artimus/ezhttp/App.java
@@ -0,0 +1,13 @@
+package com.artimus.ezhttp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
